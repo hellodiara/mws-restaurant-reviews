41d8b4792e2229c519aeb439224410f9f2a8902c
Diara-Bamba <diarabamba@gmail.com>
2018-08-24 14:18:48 -0400

    Added Mapbox + Key

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiaGVsbG9kaWFyYSIsImEiOiJjamw4OWxuNDEzZ2hjM3VwZ3lodzN4cGpvIn0.H8Cxd1LazKqHQ-uF0Ni5LQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
