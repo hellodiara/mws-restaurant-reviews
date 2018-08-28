5362eac297b0002ffd8dbe9affa4d98b1cedf446
Diara-Bamba <diarabamba@gmail.com>
2018-08-28 15:48:03 -0400

    Added div

--- js/main.js
+++ js/main.js
@@ -158,28 +158,35 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
-  const image = document.createElement('img');
-  image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  li.append(image);
-
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
   li.append(name);
 
+  const image = document.createElement('img');
+  image.className = 'restaurant-img';
+  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `${restaurant.name} restaurant`;
+  li.append(image);
+
+  const infoDiv = document.createElement('div');
+  infoDiv.className = 'info-Div';
+
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  infoDiv.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  infoDiv.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex = '3';
-  li.append(more)
+  // li.append(more)
+  infoDiv.append(more);
+
+  li.append(infoDiv);
 
   return li
 }
