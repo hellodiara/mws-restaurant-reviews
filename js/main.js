e68eab242e1657dd6463ce753f28d057fa4bb223
Diara-Bamba <diarabamba@gmail.com>
2018-09-08 10:50:34 -0400

    Added Aria Roles

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
